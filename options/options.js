2fe0f84cd1281837181f9697d6a0244a2b52a2f2
Crowdbotics <team@crowdbotics.com>
2024-02-05 04:51:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jimmyjohns-sandwich-45868",
+  url: "https://jimmyjohns-sandwich-45868.botics.co",
+  api: "https://jimmyjohns-sandwich-45868.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
